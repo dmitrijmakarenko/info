e14a936892d889c6373ceacd151b44d46acdb3fa
Dmitry Makarenko <makarenko@itoolabs.com>
2016-03-25 12:58:57 +0300

    copy to/from file

---
+++ sql/acs_copy_from_file.sql
@@ -0,0 +1,35 @@
+﻿CREATE OR REPLACE FUNCTION acs_copy_from_file()
+ RETURNS void AS
+$BODY$
+DECLARE
+json_data json;
+item json;
+r record;
+table_name text;
+BEGIN
+
+DROP TABLE IF EXISTS acs.transfer_data_t;
+
+CREATE TABLE acs.transfer_data_t (
+  id serial primary key,
+  tname text NOT NULL,
+  data json
+);
+
+COPY acs.transfer_data_t FROM '/tmp/transfer_data.copy';
+
+FOR table_name,json_data IN SELECT tname,data FROM acs.transfer_data_t
+   LOOP
+	--RAISE notice 'tname %', table_name;
+	FOR item IN SELECT * FROM json_array_elements(json_data)
+	LOOP
+		RAISE NOTICE 'item %', item;
+		EXECUTE 'INSERT INTO '|| table_name ||' SELECT * FROM json_to_record('|| quote_literal(item) ||')';
+	END LOOP;
+   END LOOP;
+
+--DROP TABLE IF EXISTS acs.transfer_data_t;
+
+END;
+$BODY$
+ LANGUAGE plpgsql;
---
+++ sql/acs_copy_to_file.sql
@@ -0,0 +1,30 @@
+﻿CREATE OR REPLACE FUNCTION acs_copy_to_file()
+ RETURNS void AS
+$BODY$
+DECLARE
+json_data json;
+tname text;
+BEGIN
+
+DROP TABLE IF EXISTS acs.transfer_data;
+
+CREATE TABLE acs.transfer_data (
+  id serial primary key,
+  tname text NOT NULL,
+  data json
+);
+
+FOR tname IN SELECT table_name FROM acs.vcs_tables
+   LOOP
+	EXECUTE 'SELECT json_agg(t) FROM (SELECT * FROM '|| tname ||') t' INTO json_data;
+	INSERT INTO acs.transfer_data(tname, data) VALUES(tname,json_data);
+	--RAISE notice 'json data %', json_data;
+   END LOOP;
+
+COPY acs.transfer_data TO '/tmp/transfer_data.copy';
+
+--DROP TABLE IF EXISTS acs.transfer_data;
+
+END;
+$BODY$
+ LANGUAGE plpgsql;
