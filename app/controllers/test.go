c11d8dc7f3eb620dbff7a87fc1e417f408b9c0ba
Dmitry Makarenko <makarenko@itoolabs.com>
2016-03-27 19:42:04 +0300

    test

--- app/controllers/test.go
+++ app/controllers/test.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/robfig/revel"
+	"database/sql"
 )
 
 type TestCntl struct {
@@ -26,6 +27,47 @@ func (c TestCntl) Reset() revel.Result {
 		ret["error"] = err.Error()
 		return c.RenderJson(ret)
 	}
+
+	//connect to general
+	connstring := "host=" + DB_HOST + " port=" + DB_PORT + " user=" + DB_USER+ " dbname=general password=" + DB_PASSWORD + " sslmode=disable"
+	dbGeneral, err := sql.Open(DB_DRIVER, connstring)
+	//connect to station1
+	connstring = "host=" + DB_HOST + " port=" + DB_PORT + " user=" + DB_USER+ " dbname=general password=" + DB_PASSWORD + " sslmode=disable"
+	dbStat1, err := sql.Open(DB_DRIVER, connstring)
+	if err != nil {
+		ret["error"] = err.Error()
+		return c.RenderJson(ret)
+	}
+
+	//create data tables
+	_, err = dbGeneral.Exec("CREATE TABLE")
+	_, err = dbStat1.Exec("CREATE TABLE")
+
+	//insert data
+
+	return c.RenderJson(ret)
+}
+
+func (c TestCntl) Init() revel.Result {
+	var err error
+	ret := make(map[string]string)
+
+	//connect to general
+	connstring := "host=" + DB_HOST + " port=" + DB_PORT + " user=" + DB_USER+ " dbname=general password=" + DB_PASSWORD + " sslmode=disable"
+	dbGeneral, err := sql.Open(DB_DRIVER, connstring)
+	//connect to station1
+	connstring = "host=" + DB_HOST + " port=" + DB_PORT + " user=" + DB_USER+ " dbname=general password=" + DB_PASSWORD + " sslmode=disable"
+	dbStat1, err := sql.Open(DB_DRIVER, connstring)
+	if err != nil {
+		ret["error"] = err.Error()
+		return c.RenderJson(ret)
+	}
+
+	//install acs
+	_, err = dbGeneral.Exec("CREATE TABLE")
+	_, err = dbStat1.Exec("CREATE TABLE")
+
+	//init
 
 	return c.RenderJson(ret)
 }
